a4ff1e240d4b90df3a94e946be78f0300037bdd0
GuaHsu <guaswork@gmail.com>
2019-02-02 23:35:06 +0800

    Add post select method

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -8,7 +8,8 @@ import './Blog.css'
 
 class Blog extends Component {
   state = {
-    posts: []
+    posts: [],
+    selectedPostId: null
   }
 
   componentDidMount () {
@@ -25,12 +26,17 @@ class Blog extends Component {
       })
   }
 
+  postSelectedHandler = (id) => {
+    this.setState({ selectedPostId: id })
+  }
+
   render () {
     const posts = this.state.posts.map(post => {
       return (
         <Post
           title={post.title}
           author={post.author}
+          clicked={() => this.postSelectedHandler(post.id)}
           key={post.id} />
       )
     })
@@ -41,7 +47,7 @@ class Blog extends Component {
           {posts}
         </section>
         <section>
-          <FullPost />
+          <FullPost id={this.state.selectedPostId} />
         </section>
         <section>
           <NewPost />
