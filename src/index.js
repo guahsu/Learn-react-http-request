1fececc30378854866caddc5079e015a698bf17b
GuaHsu <guaswork@gmail.com>
2019-02-03 00:40:26 +0800

    Add Axios interceptors to excute code globaily

--- src/index.js
+++ src/index.js
@@ -5,9 +5,30 @@ import App from './App'
 import * as serviceWorker from './serviceWorker'
 import axios from 'axios'
 
-axios.interceptors.request.use(request => {
-  console.log(request)
+const instance = axios.create({
+  baseURL: 'http://localhost:3000',
+  timeout: 10000,
+  params: {}
 })
+
+const myRequestInterceptors = axios.interceptors.request.use(request => {
+  console.log('interceptors-request:', request)
+  return request
+}, error => {
+  console.log('interceptors-request-error:', error)
+  return Promise.reject(error)
+})
+
+const myResponseInterceptors = axios.interceptors.response.use(response => {
+  console.log('interceptors-response:', response)
+  return response
+}, error => {
+  console.log('interceptors-response-error:', error)
+  return Promise.reject(error)
+})
+
+instance.interceptors.request.eject(myRequestInterceptors)
+instance.interceptors.response.eject(myResponseInterceptors)
 
 ReactDOM.render(<App />, document.getElementById('root'))
 serviceWorker.unregister()
